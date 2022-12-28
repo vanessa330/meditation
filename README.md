61b9e0f690b62a6b1b3b8e5ed19a8211db595f94
vanessa330 <vanessa.33010@gmail.com>
2022-12-28 12:08:15 +0800

    using jQuery

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Meditaiton App
-> Using bootstrap
+
+> bootstrap and jQuery
--- app.js
+++ app.js
@@ -1,82 +1,89 @@
-const playBtn = document.querySelector(".play-btn");
-let audio = document.querySelector("#audio");
-let timeDisplay = document.querySelector(".time-display");
-let fakeDuration = 300; // Default 300 = 5mins
-const timeSelect = Array.from(document.querySelectorAll(".time-select"));
-const soundSelect = Array.from(document.querySelectorAll(".sound-select"));
+$(document).ready(function () {
+  // Global variables
 
-// Select the time druration button
+  const startBtn = $("#start-btn");
+  const playPauseBtn = $("#play-pause-btn");
+  const resetBtn = $("#reset-btn");
 
-timeSelect.map((button) => {
-  button.addEventListener("click", () => {
-    fakeDuration = button.getAttribute("data-time");
+  let time = 300;
+  let soundPath = "./sounds/beach.mp3";
+  let audio = $("#audio");
+  let playingStatus = true;
+  let timerInterval = null;
+  let timeDisplay = $("#time-display");
 
-    if (fakeDuration < 600) {
-      timeDisplay.textContent = `0${fakeDuration / 60}:00`;
+  // Select the duration and the sound
+
+  function selectTime() {
+    time = $("select#time-options").val();
+  }
+
+  function selectSound() {
+    soundPath = $("select#sound-options").val();
+  }
+
+  $("select#time-options").change(selectTime);
+  $("select#sound-options").change(selectSound);
+
+  // Start , play, pause and reset buttons
+
+  startBtn.click(() => {
+    $("#duration").attr("data-time", time);
+    audio.attr("src", soundPath);
+
+    $("#timer").css("display", "block");
+    $("#selection").css("display", "none");
+
+    timerInterval = setInterval(countdown, 1000);
+    audio.trigger("play");
+  });
+
+  playPauseBtn.click(() => {
+    if (playingStatus === true) {
+      playPauseBtn.html(`<i class="fa-solid fa-play display-3"></i>`);
+      audio.trigger("pause");
+      playingStatus = false;
+      clearInterval(timerInterval);
     } else {
-      timeDisplay.textContent = `${fakeDuration / 60}:00`;
+      playPauseBtn.html(`<i class="fa-solid fa-pause display-3"></i>`);
+      audio.trigger("play");
+      playingStatus = true;
+      timerInterval = setInterval(countdown, 1000);
     }
   });
-});
 
-// Select the different audio
-
-soundSelect.map((button) => {
-  button.addEventListener("click", () => {
-    let soundPath = button.getAttribute("data-audio");
-    
-    audio.setAttribute("src", soundPath);
-    audio.load();
+  resetBtn.click(function () {
+    window.location.href = "index.html";
   });
+
+  // Animated the time display
+  function countdown() {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    let leadingSeconds = 0;
+    let leadingMinutes = 0;
+
+    if (minutes < 10) {
+      leadingMinutes = "0" + minutes.toString();
+    } else {
+      leadingMinutes = minutes;
+    }
+
+    if (seconds < 10) {
+      leadingSeconds = "0" + seconds.toString();
+    } else {
+      leadingSeconds = seconds;
+    }
+
+    timeDisplay.text(`${leadingMinutes} : ${leadingSeconds}`);
+
+    time--;
+
+    if (time === -1) {
+      audio.trigger("pause");
+      clearInterval(timerInterval);
+      timeDisplay.text("00 : 00");
+      playPauseBtn.css("display", "none");
+    }
+  }
 });
-
-// Stop and play the sound
-
-playBtn.addEventListener("click", () => {
-  checkPlaying();
-});
-
-function checkPlaying() {
-  if (audio.paused) {
-    playBtn.innerHTML = `<i class="fa-solid fa-pause display-3"></i>`;
-    audio.play();
-    timerInterval = window.setInterval(countdown, 1000);
-  } else {
-    playBtn.innerHTML = `<i class="fa-solid fa-play display-3"></i>`;
-    audio.pause();
-    window.clearInterval(timerInterval);
-  }
-}
-
-// Animated the time display
-
-function countdown() {
-  let timeRemaining = fakeDuration;
-
-  fakeDuration--;
-  let minutes = Math.floor(timeRemaining / 60);
-  let seconds = Math.floor(timeRemaining % 60);
-
-  let leadingSeconds = 0;
-  let leadingMinutes = 0;
-
-  if (minutes < 10) {
-    leadingMinutes = "0" + minutes.toString();
-  } else {
-    leadingMinutes = minutes;
-  }
-
-  if (seconds < 10) {
-    leadingSeconds = "0" + seconds.toString();
-  } else {
-    leadingSeconds = seconds;
-  }
-
-  timeDisplay.textContent = `${leadingMinutes}:${leadingSeconds}`;
-
-  if (timeRemaining <= 0) {
-    timeDisplay.textContent = `05:00`;
-    fakeDuration = 300;
-    checkPlaying();
-  }
-}
